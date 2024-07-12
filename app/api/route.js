e9b6abf6d189f8c46977f83e1a5b9a2d95fc5777
Reinhardt <56647015+Reinhardt254@users.noreply.github.com>
2024-07-12 08:24:06 +0300

    updated button paddings

--- app/api/route.js
+++ app/api/route.js
@@ -1,23 +1,23 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
-export async function POST(){
-   try{
-      const data = await prismadb.post();
-   }catch(error){
-      console.log(error)
-   }
+// export async function POST(){
+//    try{
+//       const data = await prismadb.post();
+//    }catch(error){
+//       console.log(error)
+//    }
 
-   return NextResponse.json("Success") 
-}
+//    return NextResponse.json("Success") 
+// }
 
-export async function GET(){
+// export async function GET(){
 
-}
+// }
 
-export async function PATCH(){
+// export async function PATCH(){
    
-}
+// }
 
-export async function DELETE(){
+// export async function DELETE(){
 
-}
+// }
